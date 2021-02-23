c3f58971784e11396f3585f3aa53046dd2d6140c
Jamesh <jamesh39@outlook.com>
2021-02-23 13:05:30 -0800

    corrected TEXT property which was set to STRING for courses model

--- models/course.js
+++ models/course.js
@@ -20,7 +20,7 @@ module.exports = (sequelize) => {
             }
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 notNull: {
